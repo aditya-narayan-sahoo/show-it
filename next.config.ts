3b4b889a9668e1ee44ba49864738c753e06029f6
Aditya Narayan Sahoo <adityasahoo246@gmail.com>
2024-11-11 09:39:56 +0530

    Add sanity write client

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  dangerouslyAllowSVG: true,
+
   images: {
     remotePatterns: [{ protocol: "https", hostname: "*" }],
+  },
+  experimental: {
+    after: true,
   },
   devIndicators: {
     appIsrStatus: true,
